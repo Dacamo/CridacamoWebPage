6aa333dd37e16b4a89fcaeecc71b58431270ab65
Cristian David Castro Montes <cristian.castro5840@uco.net.co>
2018-07-01 20:07:40 -0500

    Project Base Code

---
+++ CridacamoWebPage/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CridacamoWebPage.MvcApplication" Language="C#" %>
---
+++ CridacamoWebPage/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CridacamoWebPage.Startup))]
+namespace CridacamoWebPage
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
