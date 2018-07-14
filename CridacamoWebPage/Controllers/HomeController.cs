ce33877bc1498a5fdec3d24f3c04c863e0a49a70
Cristian David Castro Montes <cristian.castro5840@uco.net.co>
2018-07-14 01:57:34 -0500

    Partial views are added

--- CridacamoWebPage/Controllers/HomeController.cs
+++ CridacamoWebPage/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
-﻿using System;
+﻿
+using CridacamoWebPage.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+
+
+
 
 namespace CridacamoWebPage.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+       
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +35,25 @@ namespace CridacamoWebPage.Controllers
 
             return View();
         }
+
+        public ActionResult ShowAllpost()
+        {
+            var Post = db.Posts.ToList();
+            return PartialView("_ShowAllpost", Post);
+        }
+
+        public ActionResult Show(string id)
+        { 
+            var post = db.Posts.SqlQuery("SELECT * FROM dbo.Posts WHERE Author = @value", new SqlParameter("@value", id));
+            if (post==null)
+            {
+                return HttpNotFound();
+            }
+            return PartialView("_Show", post);
+        }
+
+        
+
+
     }
 }
